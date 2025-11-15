41fd3a863cb8b79734917a78b349e90ec31bf45d
Patrick Alvarez Eades <p.alvarezeades@gmail.com>
2025-11-15 18:10:25 +0100

    Category list item updated ui

--- src/presentation/components/settings/categories/CategoryListItem.tsx
+++ src/presentation/components/settings/categories/CategoryListItem.tsx
@@ -1,6 +1,14 @@
 import type { Category } from '@/src/domain/model/Category';
 import { getIconComponent } from '@/src/shared/utils/icons';
-import { Item, ItemContent, ItemDescription, ItemTitle } from '../../ui/item';
+import { PencilIcon, TrashIcon } from 'lucide-react';
+import { Button } from '../../ui/button';
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemMedia,
+  ItemTitle,
+} from '../../ui/item';
 
 interface CategoryListItemProps {
   category: Category;
@@ -11,19 +19,25 @@ export const CategoryListItem = ({ category }: CategoryListItemProps) => {
   const IconComponent = getIconComponent(category.icon);
 
   return (
-    <Item
-      variant="outline"
-      className="w-full hover:bg-accent/50"
-      style={{ backgroundColor: color }}
-    >
-      <ItemContent className="flex flex-row gap-4 items-center justify-between transition-colors duration-100">
-        <div className="flex flex-row gap-4 items-center">
-          <ItemTitle className="text-sm font-medium">{category.name}</ItemTitle>
-          <ItemDescription className="text-sm text-muted-foreground flex items-center">
-            <IconComponent className="size-4" />
-          </ItemDescription>
-        </div>
+    <Item variant="outline" className="p-0 w-full hover:bg-accent/50">
+      <ItemMedia
+        className="h-full py-2 px-4 border border-border rounded-l-md"
+        style={{ backgroundColor: color }}
+      >
+        <IconComponent className="size-5" />
+      </ItemMedia>
+
+      <ItemContent className="flex flex-row gap-4 items-center justify-between transition-colors duration-100 p-4">
+        <ItemTitle className="text-sm font-medium">{category.name}</ItemTitle>
       </ItemContent>
+      <ItemActions className="flex flex-row gap-4 items-center justify-end transition-colors duration-100 p-4">
+        <Button variant="ghost" size="icon">
+          <PencilIcon className="size-4" />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <TrashIcon className="size-4" />
+        </Button>
+      </ItemActions>
     </Item>
   );
 };
